81df25c0f32777647dae722f65c9b9c8aa08aafc
Ryosuke Hasebe <ryosuke.hasebe@linecorp.com>
2015-05-08 18:04:21 +0900

    fix test

--- src/test/java/com/be_hase/honoumi/netty/server/ServerTest.java
+++ src/test/java/com/be_hase/honoumi/netty/server/ServerTest.java
@@ -2,8 +2,6 @@ package com.be_hase.honoumi.netty.server;
 
 import java.util.List;
 
-import junit.framework.TestCase;
-
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,13 +12,15 @@ import com.be_hase.honoumi.routing.Router;
 import com.google.common.collect.Lists;
 import com.google.inject.AbstractModule;
 
+import junit.framework.TestCase;
+
 public class ServerTest extends TestCase {
 	@Before
 	public void setUp() {
 		System.setProperty("application.environment", "local");
 		System.setProperty("application.properties", "test1.properties,test2.properties,server.properties");
 	}
-	
+
 	@Test
 	public void test_create() {
 		Router router = new Router();
@@ -31,19 +31,19 @@ public class ServerTest extends TestCase {
 		modules.add(new TestModule());
 		Server testServer = Server.create("testServer", router, modules);
 		testServer.start();
-		
+
 		MonitoringServer monitoringServer = MonitoringServer.create(testServer);
 		monitoringServer.start();
-		
+
 		assertEquals("testServer", testServer.getServerName());
 		assertEquals(22222, testServer.getPort());
 		assertEquals("UTF-8", testServer.getCharsetStr());
 		assertEquals("true", testServer.getServerBootstrap().getOption("reuseAddress").toString());
 		assertEquals("true", testServer.getServerBootstrap().getOption("child.keepAlive").toString());
 		assertEquals("true", testServer.getServerBootstrap().getOption("child.tcpNoDelay").toString());
-		
+
 		assertEquals("hoge", HttpUtilForTest.get("http://localhost:22222/hoge", null).getResponse());
 		assertEquals("12", HttpUtilForTest.get("http://localhost:22222/bar/1/2", null).getResponse());
-		assertEquals("{\"path2\":\"2\",\"path1\":\"1\"}", HttpUtilForTest.get("http://localhost:22222/fuga/1/2", null).getResponse());
+		assertEquals("{\"path1\":\"1\",\"path2\":\"2\"}", HttpUtilForTest.get("http://localhost:22222/fuga/1/2", null).getResponse());
 	}
 }
--- src/test/java/com/be_hase/honoumi/routing/RouterTest.java
+++ src/test/java/com/be_hase/honoumi/routing/RouterTest.java
@@ -1,8 +1,9 @@
 package com.be_hase.honoumi.routing;
 
-import java.util.Map;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import junit.framework.TestCase;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,15 +11,17 @@ import org.junit.Test;
 import com.be_hase.honoumi.controller.TestController;
 import com.google.common.collect.Maps;
 
+import junit.framework.TestCase;
+
 public class RouterTest extends TestCase {
 	//private static final Logger logger = LoggerFactory.getLogger(RouterTest.class);
-	
+
 	@Before
 	public void setUp() {
 		System.setProperty("application.environment", "local");
 		System.setProperty("application.properties", "test1.properties,test2.properties,server.properties");
 	}
-	
+
 	@Test
 	public void test_getRouteFor() {
 		Router router = new Router();
@@ -26,11 +29,11 @@ public class RouterTest extends TestCase {
 		router.POST().route("/blog/{blogId}/edit").with(TestController.class, "bar");
 		router.GET().route("/events/.*").with(TestController.class, "fuga");
 		router.compileRoutes();
-		
+
 		Route route1 = router.getRouteFor("GET", "/etc/privacy?hoge=hoge&bar=bar");
 		Route route2 = router.getRouteFor("POST", "/blog/1234/edit");
 		Route route3 = router.getRouteFor("GET", "/events/sdvnwbgp/vsjgowe/dsjv");
-		
+
 		assertTrue(route1.getControllerClass().getSimpleName().equals("TestController"));
 		assertTrue(route1.getControllerMethod().getName().equals("hoge"));
 		assertTrue(route2.getControllerClass().getSimpleName().equals("TestController"));
@@ -38,7 +41,7 @@ public class RouterTest extends TestCase {
 		assertTrue(route3.getControllerClass().getSimpleName().equals("TestController"));
 		assertTrue(route3.getControllerMethod().getName().equals("fuga"));
 	}
-	
+
 	@Test
 	public void test_getReverseRoute() {
 		Router router = new Router();
@@ -46,25 +49,25 @@ public class RouterTest extends TestCase {
 		router.POST().route("/blog/{blogId}/edit").with(TestController.class, "bar");
 		router.GET().route("/events/.*").with(TestController.class, "fuga");
 		router.compileRoutes();
-		
+
 		Map<String, Object> map = Maps.newHashMap();
 		map.put("blogId", "1234");
 		map.put("huga", "hughuga");
-		
+
 		String uri1 = router.getReverseRoute(TestController.class, "hoge");
 		String uri2 = router.getReverseRoute(TestController.class, "hoge", map);
 		String uri3 = router.getReverseRoute(TestController.class, "bar");
 		String uri4 = router.getReverseRoute(TestController.class, "bar", map);
 		String uri5 = router.getReverseRoute(TestController.class, "fuga");
 		String uri6 = router.getReverseRoute(TestController.class, "fuga", map);
-		
-		assertTrue(uri1.equals("/etc/privacy"));
-		assertTrue(uri2.equals("/etc/privacy?blogId=1234&huga=hughuga"));
-		
-		assertTrue(uri3.equals("/blog/{blogId}/edit"));
-		assertTrue(uri4.equals("/blog/1234/edit?huga=hughuga"));
-		
-		assertTrue(uri5.equals("/events/.*"));
-		assertTrue(uri6.equals("/events/.*?blogId=1234&huga=hughuga"));
+
+		assertThat(uri1, is("/etc/privacy"));
+		assertThat(uri2, is("/etc/privacy?huga=hughuga&blogId=1234"));
+
+		assertThat(uri3, is("/blog/{blogId}/edit"));
+		assertThat(uri4, is("/blog/1234/edit?huga=hughuga"));
+
+		assertThat(uri5, is("/events/.*"));
+		assertThat(uri6, is("/events/.*?huga=hughuga&blogId=1234"));
 	}
 }
